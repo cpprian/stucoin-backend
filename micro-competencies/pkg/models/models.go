83e1e0e96bb880eee1a31a4a390bd8a9576f0426
cpprian <cpprian456@gmail.com>
2024-01-28 21:12:04 +0100

    delete unecessary imports

--- micro-competencies/pkg/models/models.go
+++ micro-competencies/pkg/models/models.go
@@ -1,10 +1,6 @@
 package models
 
-import (
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
+import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type MicroCompetence struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty"`
